f25a1be4e7f6bb62651f177b1dea236780f0fc3a
silviaclaire <silviaclaire@gmail.com>
2020-03-26 00:24:11 +0900

    add: k8s manifest scripts

---
+++ bin/deploy_k8s_app.sh
@@ -0,0 +1,24 @@
+
+# ref: https://github.com/ianlewis/kubernetes-bluegreen-deployment-tutorial
+
+# Create Blue deployment
+sed 's/TAGVERSION/1.0/' kubernetes/deployment.yaml | kubectl apply -f -
+
+# Create the service
+sed 's/TAGVERSION/1.0/' kubernetes/service.yaml | kubectl apply -f -
+
+# Test Blue
+EXTERNAL_IP=$(kubectl get svc hello-app -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+curl -s http://$EXTERNAL_IP/
+
+# Update App
+
+# Create Green deployment
+sed 's/TAGVERSION/1.1/' kubernetes/deployment.yaml | kubectl apply -f -
+
+# Switch traffic to Green
+sed 's/TAGVERSION/1.1/' kubernetes/service.yaml | kubectl apply -f -
+
+# Test Green
+EXTERNAL_IP=$(kubectl get svc hello-app -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+curl -s http://$EXTERNAL_IP/
