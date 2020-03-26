e383c82de559c01ac914efbd8a0218d1dc0c6658
silviaclaire <silviaclaire@gmail.com>
2020-03-27 00:31:21 +0900

    fix: wait for deployment ready

--- bin/deploy_k8s_app.sh
+++ bin/deploy_k8s_app.sh
@@ -1,17 +1,21 @@
 # ref: https://github.com/ianlewis/kubernetes-bluegreen-deployment-tutorial
+# jsonpath: https://kubernetes.io/docs/reference/kubectl/jsonpath/
 
-DEPLOY_VERSION=$1
-SVC_NAME='hello-app'
+VERSION=$1
+SERVICE='hello-app'
+DEPLOYMENT=$SERVICE-$1
 
 # Always create a new deployment
-sed "s/TAGVERSION/$DEPLOY_VERSION/" k8s/deployment.yaml | kubectl apply -f -
+sed "s/TAGVERSION/$VERSION/" k8s/deployment.yaml | kubectl apply -f -
+
+# Wait until the Deployment is ready by checking the MinimumReplicasAvailable condition.
+kubectl wait --for=condition=available --timeout=600s deployment/$DEPLOYMENT
 
 # Create/Update service that expose this deployment
-# In the case of an update, traffic will be switched to the new deployment.
-sed "s/TAGVERSION/$DEPLOY_VERSION/" k8s/service.yaml | kubectl apply -f -
+# In the case of an update, traffic will be switched to the new version.
+sed "s/TAGVERSION/$VERSION/" k8s/service.yaml | kubectl apply -f -
 
 # Test
-# ref: https://kubernetes.io/docs/reference/kubectl/jsonpath/
-EXTERNAL_HOSTNAME=$(kubectl get svc $SVC_NAME -o=jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+EXTERNAL_HOSTNAME=$(kubectl get svc $SERVICE -o=jsonpath="{.status.loadBalancer.ingress[*].hostname}")
 echo `http://$EXTERNAL_HOSTNAME/`
 curl -s http://$EXTERNAL_HOSTNAME/
