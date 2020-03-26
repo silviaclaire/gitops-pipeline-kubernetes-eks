4d4d2172b28a6e615b42d56386f97b43bb49f4bf
Claire Yan <silviaclaire@gmail.com>
2020-03-26 00:19:56 +0000

    add: install aws-iam-authenticator

--- bin/setup_jenkins_ubuntu.sh
+++ bin/setup_jenkins_ubuntu.sh
@@ -51,6 +51,14 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
 
+# install aws-iam-authenticator
+# ref: https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html#linux
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws-iam-authenticator help
+
 # install pip
 sudo apt-get install python3-pip
 pip3 --version
