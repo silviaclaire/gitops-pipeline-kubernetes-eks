194acf75f99db2c37c337e472cf16527aed0bc68
yan <yan-xintong@nippon-control-system.co.jp>
2020-03-26 12:48:51 +0900

    update: CLUSTER_NAME as parameter

--- bin/create_cluster_eks.sh
+++ bin/create_cluster_eks.sh
@@ -1,6 +1,7 @@
 # ref: https://docs.aws.amazon.com/eks/latest/userguide/getting-started-console.html
 
 AWS_REGION=$1
+CLUSTER_NAME=$2
 
 # chmod +x bin/create_or_update_stack.sh
 
@@ -11,10 +12,11 @@ AWS_REGION=$1
 ./bin/create_or_update_stack.sh $AWS_REGION eks-vpc cloudformation/vpc.yaml
 
 # Create Amazon EKS Cluster
+sed -i "s/CLUSTER_NAME/$CLUSTER_NAME/" cloudformation/cluster.yaml
 ./bin/create_or_update_stack.sh $AWS_REGION eks-cluster cloudformation/cluster.yaml
 
 # Create/Update kubeconfig File for cluster
-aws eks update-kubeconfig --region $AWS_REGION --name aws-eks-cluster
+aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 
 # Test the configuration
 kubectl get svc
