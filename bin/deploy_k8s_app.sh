192c1d9a04fa0feed2f4ddfcb7fda01969ae538c
yan <yan-xintong@nippon-control-system.co.jp>
2020-03-27 13:06:50 +0900

    refactor: add check status function

--- bin/deploy_k8s_app.sh
+++ bin/deploy_k8s_app.sh
@@ -6,32 +6,30 @@ NAMESPACE=$2  # use branch name
 SERVICE='hello-app'
 DEPLOYMENT=$SERVICE-$1
 
+check_status () {
+    kubectl get services -n ${NAMESPACE}
+    kubectl get deployments -n ${NAMESPACE}
+    kubectl get pods -n ${NAMESPACE}
+}
+
 # Create namespace if it doesn't exist
 kubectl get ns ${NAMESPACE} || kubectl create ns ${NAMESPACE}
 
 # Always create a new deployment
 sed "s/TAGVERSION/$VERSION/" k8s/${NAMESPACE}/deployment.yaml | kubectl -n ${NAMESPACE} apply -f -
-
-# Check
-kubectl get services -n ${NAMESPACE}
-kubectl get deployments -n ${NAMESPACE}
-kubectl get pods -n ${NAMESPACE}
+check_status
 
 # Wait until the Deployment is ready by checking the MinimumReplicasAvailable condition.
 kubectl wait --for=condition=available --timeout=120s -n ${NAMESPACE} deployment/$DEPLOYMENT
-
-# Check again
-kubectl get services -n ${NAMESPACE}
-kubectl get deployments -n ${NAMESPACE}
-kubectl get pods -n ${NAMESPACE}
+check_status
 
 # Create/Update service that expose this deployment
 # In the case of an update, traffic will be switched to the new version.
 sed "s/TAGVERSION/$VERSION/" k8s/${NAMESPACE}/service.yaml | kubectl -n ${NAMESPACE} apply -f -
-
 # Wait a few seconds for the service become ready
 # ref: For now Services do not have conditions in their status. (kubenetes #80828)
 sleep 60
+check_status
 
 # Test
 EXTERNAL_HOSTNAME=$(kubectl get svc $SERVICE -n ${NAMESPACE} -o=jsonpath="{.status.loadBalancer.ingress[*].hostname}")
